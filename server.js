96ca6d6ac13d15f06633650b6cff4a63ab46c87e
HarshaMacherla <macherlaharsha997@gmail.com>
2023-12-08 11:56:02 +0530

    Added routes to the Express JS

--- server.js
+++ server.js
@@ -30,40 +30,19 @@ app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: false }));
 
+// Built-in middleware for json
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+// Serve static files
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/subdir", express.static(path.join(__dirname, "public")));
 
-app.get("^/$|index(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-});
+// Routes
+app.use("/", require("./routes/root"));
+app.use("/subdir", require("./routes/subdir"));
+app.use("/employees", require("./routes/api/employees"));
 
-app.get("/new-page(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
-
-app.get("/old-page(.html)?", (req, res) => {
-  res.redirect(301, "/new-page.html");
-});
-
-// Route handlers
-const one = (req, res, next) => {
-  console.log("One");
-  next();
-};
-
-const two = (req, res, next) => {
-  console.log("Three");
-  next();
-};
-
-const three = (req, res, next) => {
-  console.log("three");
-  res.send("Finished");
-};
-
-app.get("/chain(.html)?", [one, two, three]);
-
+// 404 Error Page
 app.all("*", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
